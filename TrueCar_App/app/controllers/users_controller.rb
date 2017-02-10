d040ac2b9c75c6cbcd16c484c74aa835c0bb0770
Patrick M Hulligan <hulligap@onid.orst.edu>
2017-02-09 19:50:40 -0800

    Added pictures and started adding follow feature

---
+++ TrueCar_App/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def index
+  	@users = User.all
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to users_path, :notice => "Post has been created under given user"
+  	else
+  		render "new"
+  	end
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def user_params
+  	params.require(:user).permit(:name, :password)
+  end
+end
--- TrueCar_App/config/routes.rb
+++ TrueCar_App/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/create'
+
+  get 'users/new'
+
+  get 'users/following_params'
+
   get 'followings/index'
 
   get 'followings/new'
@@ -20,6 +28,8 @@ Rails.application.routes.draw do
   resources :posts
 
   resources :followings
+
+  resources :users
   root :to => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
